16856803a3834479abf0462e98415e0452e2de26
Avais Pagarkar <avaispagarkar123@gmail.com>
2016-09-24 18:45:58 +0530

    Created json object for charts api

--- hello/views.py
+++ hello/views.py
@@ -10,7 +10,7 @@ import numpy as np
 from sklearn import datasets, linear_model
 
 from .models import Usermodel
-
+import json
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
@@ -60,6 +60,116 @@ def post_login(request):
 def invalidLogin(request):
 	return render(request,'invalidLogin.html')
 
+def after_login(request):
+	f = open("data/account1.json","r")
+	i = 0
+	json_obj = {}
+	balance = 500000
+	for line in f:
+		line_json = json.loads(line)
+		line_obj = {}
+		final_json = '['
+		for i in range(0,len(line_json)):
+			json_obj = '{'
+			line_obj = line_json[i]
+			json_obj += "\"account\" :" + "\"" + line_obj["account1"] + "\","
+			if line_obj["transaction"] == 0:
+				balance -= line_obj["amount"]
+			else:
+				balance += line_obj["amount"]
+			json_obj += "\"balance\" :" + "\"" + str(balance) + "\","
+			json_obj += "\"date\" :" + "\"" + str(line_obj["date"]) + "\","
+			json_obj += "\"month\" :" +  "\"" + str(line_obj["month"]) + "\""
+			final_json += json_obj + "}"
+			if (i+1) != len(line_json):
+				final_json += ','
+			else:
+				final_json += ']'
+
+	obj1 = json.loads(final_json)
+
+	f = open("data/account2.json","r")
+	i = 0
+	json_obj = {}
+	balance = 500000
+	for line in f:
+		line_json = json.loads(line)
+		line_obj = {}
+		final_json = '['
+		for i in range(0,len(line_json)):
+			json_obj = '{'
+			line_obj = line_json[i]
+			json_obj += "\"account\" :" + "\"" + line_obj["account1"] + "\","
+			if line_obj["transaction"] == 0:
+				balance -= line_obj["amount"]
+			else:
+				balance += line_obj["amount"]
+			json_obj += "\"balance\" :" + "\"" + str(balance) + "\","
+			json_obj += "\"date\" :" + "\"" + str(line_obj["date"]) + "\","
+			json_obj += "\"month\" :" +  "\"" + str(line_obj["month"]) + "\""
+			final_json += json_obj + "}"
+			if (i+1) != len(line_json):
+				final_json += ','
+			else:
+				final_json += ']'
+
+	obj2 = json.loads(final_json)
+
+	f = open("data/account3.json","r")
+	i = 0
+	json_obj = {}
+	balance = 500000
+	for line in f:
+		line_json = json.loads(line)
+		line_obj = {}
+		final_json = '['
+		for i in range(0,len(line_json)):
+			json_obj = '{'
+			line_obj = line_json[i]
+			json_obj += "\"account\" :" + "\"" + line_obj["account1"] + "\","
+			if line_obj["transaction"] == 0:
+				balance -= line_obj["amount"]
+			else:
+				balance += line_obj["amount"]
+			json_obj += "\"balance\" :" + "\"" + str(balance) + "\","
+			json_obj += "\"date\" :" + "\"" + str(line_obj["date"]) + "\","
+			json_obj += "\"month\" :" +  "\"" + str(line_obj["month"]) + "\""
+			final_json += json_obj + "}"
+			if (i+1) != len(line_json):
+				final_json += ','
+			else:
+				final_json += ']'
+	obj3 = json.loads(final_json)
+
+	f = open("data/account4.json","r")
+	i = 0
+	json_obj = {}
+	balance = 500000
+	for line in f:
+		line_json = json.loads(line)
+		line_obj = {}
+		final_json = '['
+		for i in range(0,len(line_json)):
+			json_obj = '{'
+			line_obj = line_json[i]
+			json_obj += "\"account\" :" + "\"" + line_obj["account1"] + "\","
+			if line_obj["transaction"] == 0:
+				balance -= line_obj["amount"]
+			else:
+				balance += line_obj["amount"]
+			json_obj += "\"balance\" :" + "\"" + str(balance) + "\","
+			json_obj += "\"date\" :" + "\"" + str(line_obj["date"]) + "\","
+			json_obj += "\"month\" :" +  "\"" + str(line_obj["month"]) + "\""
+			final_json += json_obj + "}"
+			if (i+1) != len(line_json):
+				final_json += ','
+			else:
+				final_json += ']'
+
+	obj4 = json.loads(final_json)
+	final_obj = json.loads("["+str(obj1)+","+str(obj2)+","+str(obj3)+","+str(obj4)+"]")
+	return render(request, "dummy.html", {"obj" : final_obj})
+
 def pred_bal(month,day):
 	df = pd.read_json(path_or_buf=url)
 	df_jan = df.loc[df['month']==month]
@@ -94,9 +204,3 @@ def pred_bal(month,day):
 	#print(regr.predict(day))
 
 	return (regr.predict(day)) 
-
-
-
-
-
-
